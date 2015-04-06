2797779a8d5886f20ba099c42736f58da7b4cc18
ridger <jgjahuar@hotmail.com>
2015-04-06 16:29:19 -0500

    base del proyecto

---
+++ ejemplo/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Ridger
+ * Date: 06/04/2015
+ * Time: 04:28 p.m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace ejemplo
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
